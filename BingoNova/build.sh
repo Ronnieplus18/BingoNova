9db1e0613755eafb8a1c634496b2b20b399f98c2
Ronnie <ronito.ostaiza2004@gmail.com>
2026-07-26 01:33:57 -0500

    Crear superusuario automático

--- BingoNova/build.sh
+++ BingoNova/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crea el superusuario automáticamente si no existe (usando variables de entorno).
+# Si ya existe, este comando falla silenciosamente gracias al "|| true".
+python manage.py createsuperuser --no-input || true
